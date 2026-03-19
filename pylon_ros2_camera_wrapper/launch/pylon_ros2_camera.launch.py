efbeee0ecd6a06b4124dd17a594a57f3d26c532f
Bharath Sivaram <bharathsivaram10@gmail.com>
2026-03-19 13:34:31 -0500

    ffmpeg settings

--- pylon_ros2_camera_wrapper/launch/pylon_ros2_camera.launch.py
+++ pylon_ros2_camera_wrapper/launch/pylon_ros2_camera.launch.py
@@ -60,7 +60,23 @@ def _launch_node(context: LaunchContext):
                         'gige/mtu_size': mtu_size,
                         'startup_user_set': startup_user_set,
                         'enable_status_publisher': enable_status_publisher,
-                        'enable_current_params_publisher': enable_current_params_publisher
+                        'enable_current_params_publisher': enable_current_params_publisher,
+
+                        # ffmpeg raw params
+                        'pylon_ros2_camera_node.image_raw.ffmpeg.encoder': "h264_nvenc",
+                        'pylon_ros2_camera_node.image_raw.ffmpeg.bit_rate': 4000000,
+                        'pylon_ros2_camera_node.image_raw.ffmpeg.preset': "p1",
+                        'pylon_ros2_camera_node.image_raw.ffmpeg.tune': "ull",
+                        'pylon_ros2_camera_node.image_raw.ffmpeg.gop_size': 10,
+                        'pylon_ros2_camera_node.image_raw.ffmpeg.delay': 0,
+
+                        # ffmpeg rect params
+                        'pylon_ros2_camera_node.image_rect.ffmpeg.encoder': "h264_nvenc",
+                        'pylon_ros2_camera_node.image_rect.ffmpeg.bit_rate': 4000000,
+                        'pylon_ros2_camera_node.image_rect.ffmpeg.preset': "p1",
+                        'pylon_ros2_camera_node.image_rect.ffmpeg.tune': "ull",
+                        'pylon_ros2_camera_node.image_rect.ffmpeg.gop_size': 10,
+                        'pylon_ros2_camera_node.image_rect.ffmpeg.delay': 0
                     }
                 ]
             ),
